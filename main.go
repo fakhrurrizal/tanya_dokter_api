0486bda098b8d71a543a375047360ff772082585
fakhrurrizal10 <fakhrurrizal10@gmail.com>
2024-11-03 21:43:53 +0700

    Setup Authentication

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
-package main
+package handler
 
 import (
-	"log"
+	"net/http"
 	"tanya_dokter_app/app/router"
 	"tanya_dokter_app/config"
-	"time"
 
 	"github.com/labstack/echo/v4"
-	"gopkg.in/tylerb/graceful.v1"
 )
 
-func main() {
+// func main() {
+// 	app := echo.New()
+// 	router.Init(app)
+// 	config.Database()
+
+// 	app.Server.Addr = "127.0.0.1:" + config.LoadConfig().Port
+// 	log.Printf("Server: " + config.LoadConfig().BaseUrl)
+// 	log.Printf("Documentation: " + config.LoadConfig().BaseUrl + "/docs")
+// 	graceful.ListenAndServe(app.Server, 5*time.Second)
+
+// }
+
+func Main(w http.ResponseWriter, r *http.Request) {
+	e := Start()
+
+	e.ServeHTTP(w, r)
+}
+
+func Start() *echo.Echo {
 	app := echo.New()
-	router.Init(app)
+
 	config.Database()
 
-	app.Server.Addr = "127.0.0.1:" + config.LoadConfig().Port
-	log.Printf("Server: " + config.LoadConfig().BaseUrl)
-	log.Printf("Documentation: " + config.LoadConfig().BaseUrl + "/docs")
-	graceful.ListenAndServe(app.Server, 5*time.Second)
+	router.Init(app)
 
+	return app
 }
